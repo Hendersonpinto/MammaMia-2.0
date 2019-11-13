b0d95340eb39aab499480299429f879e9d75b192
juliana geller <bablouchka@gmail.com>
2019-11-13 15:01:56 +0100

    dashboard created and styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard', to: 'pages#dashboard', as: :dashboard
+  get '/dashboard', to: 'dashboards#dashboard', as: :dashboard
   resources :moms, only: [:show, :index, :new, :create, :destroy] do#WE NEED TO ADD CREATE LATER
     resources :bookings, only: [:create, :destroy]
   end
