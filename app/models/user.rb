cade7e78702135ac8570f1461f237af61b3e800e
Henderson Pinto <hendersoncode@gmail.com>
2020-01-08 13:05:49 +0100

    Fixing the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :mom, foreign_key: 'owner_id'
   has_many :bookings, dependent: :destroy
   has_many :moms, through: :bookings
   validates :name, uniqueness: true, presence: true
   validates :last_name, presence: true
   # validates :location, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
